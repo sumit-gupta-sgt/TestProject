e251838325698b8651fdb13ba118ba88a01a5eee
root <root@localhost.localdomain>
2018-05-18 20:49:37 +0000

    add import ita

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,7 +14,6 @@
 #
 import os
 import sys
-import "../../library/" 
 # sys.path.insert(0, os.path.abspath('.'))
 #sys.path.insert(0, os.path.abspath('../../'))
 # -- Project information -----------------------------------------------------

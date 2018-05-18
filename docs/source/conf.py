36a561824bd8eacb7d72e13c0366dda112ca87d4
root <root@localhost.localdomain>
2018-05-18 21:32:03 +0000

    add neww1

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -16,6 +16,7 @@ import os
 import sys
 # sys.path.insert(0, os.path.abspath('.'))
 sys.path.insert(0, os.path.abspath('../../library'))
+sys.path.insert(1, os.path.abspath('../../'))
 # -- Project information -----------------------------------------------------
 
 

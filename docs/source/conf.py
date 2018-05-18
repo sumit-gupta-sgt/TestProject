48c95ec4c786f9bd40ad8d838584f178a8071696
root <root@localhost.localdomain>
2018-05-18 20:08:44 +0000

    add import it

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,6 +14,7 @@
 #
 import os
 import sys
+import "../../library/" 
 # sys.path.insert(0, os.path.abspath('.'))
 #sys.path.insert(0, os.path.abspath('../../'))
 # -- Project information -----------------------------------------------------
